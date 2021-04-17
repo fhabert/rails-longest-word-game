d4b0963103cd9b63888ad7697804219ca70ac8ca
felix habert <uclqfbf@ucl.ac.uk>
2021-04-17 17:30:28 +0100

    yeah

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,46 +4,48 @@ require 'open-uri'
 class GamesController < ApplicationController
     def new
         @letters = lettersInit()
+        @started_at = Time.now.to_time
     end
     
     def score
+        ended_at = Time.now.to_time
+        time = (ended_at.to_i - params['start_time'].to_i).abs * 1000
         @word = params['word']
         @letters = params[:grid].split(' ')
-        @canBuild = can_build?(@word)
+        @canBuild = can_build?()
         @score = 0
-        @englishWord = real_word?(@word)
+        @englishWord = real_word?()
         if @canBuild && @englishWord
-            @score = @word.length
+            @score = (@word.length / time).abs
         end
-        if session['score']
-            session['score'] += @score
+        if session[:score]
+            session[:score] += @score
         else
-            session['score'] = 0
+            session[:score] = @score
         end
     end
 
-    def real_word?(word)
-        url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    private 
+
+    def real_word?
+        url = "https://wagon-dictionary.herokuapp.com/#{@word}"
         html = open(url).read
         data = JSON.parse(html)
-        if data['found']
-            return true
-        else 
-            return false
-        end
+        data['found']
     end
 
-    def can_build?(word)
+    def can_build?
         counter = 0
-        @word.each_char do |letter|
-            if @letters.include?(letter)
+        letters = @letters
+        @word.split("").each_with_index do |letter, index|
+            if letters.include?(letter)
                 counter += 1
+                letters.delete_at(index)
             end
         end
         if counter == @word.length
             return true
-        else 
-            return false
+        return false
         end
     end
 end
